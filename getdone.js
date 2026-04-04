6066fc352b54de3f87e8c4945220a779714f7aa0
Amarachi Ogueji <amarachiogueji@gmail.com>
2026-04-04 19:01:59 +0100

    Added edit & delete button, progress bar, confetti animation, saved to local storage and made the app more responsive. For now, this brings me to the end of the GetDone App.

--- getdone.js
+++ getdone.js
@@ -10,6 +10,9 @@ document.addEventListener('DOMContentLoaded', () => {
    const addTaskBtn = document.getElementById('add-task-btn');
    const taskList = document.getElementById('task-list');
    const emptyImage = document.querySelector('.empty-image');
+   const todosContainer = document.querySelector('.todos-container');
+   const progressbar = document.getElementById('progress');
+   const progressNumber = document.getElementById('numbers');
 /*
 The getElementById variables live inside DOMContentLoaded — meaning they only run after the page is fully loaded. That's their relationship.
 Also, These variables only run AFTER page loads — that's the connection
@@ -19,11 +22,40 @@ Then these variables get their OWN event listeners that let's tem perform a task
 
 const toggleEmptyState = () => {
   emptyImage.style.display = taskList.children.length === 0? 'block' : 'none';//this removes empty image when there is a task list 
+  todosContainer.style.width = taskList.children.length > 0 ?'100%' : '50%' //this ensures the container resizes based on the content.
 }
 
-const addTask = (event) => {
-    event.preventDefault();
-    const taskText = taskInput.value.trim();
+const updateProgress = (checkCompletion = true) => {
+  const totalTask = taskList.children.length;
+  const completedTasks = taskList.querySelectorAll('.checkbox:checked').length;
+
+  progressbar.style.width = totalTask ? `${(completedTasks / totalTask) * 100}%` : '0%';
+progressNumber.textContent = `${completedTasks} / ${totalTask}`;
+if(checkCompletion && totalTask > 0 && completedTasks === totalTask)  {
+    Confetti();
+
+}
+
+};
+
+const saveTaskToLocalStorage = () => {
+const tasks = Array.from(taskList.querySelectorAll('li')).map(li => ({
+text: li.querySelector('span').textContent,
+completed: li.querySelector('.checkbox').checked
+  }));
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+};
+
+
+const loadTaskFromLocalStorage = () => {
+  const savedTasks = JSON.parse(localStorage.getItem('task')) || [];
+  savedTasks.forEach(({ text, completed }) => addTask(text, completed, false));
+  toggleEmptyState();
+  updateProgress();
+};
+
+const addTask = (text, complete = false, checkCompletion = true) => {
+  const taskText = text || taskInput.value.trim();
     if(!taskText) {
         return; 
     }
@@ -34,7 +66,7 @@ const addTask = (event) => {
 
   const li = document.createElement('li');
   li.innerHTML = `
-  <input type="checkbox" class="checkbox">
+  <input type="checkbox" class="checkbox" ${complete ? 'checked' : ''}>
   <span>${taskText}</span>
   <div class="task-buttons">
   <button class= "edit-btn"><i class="fa-solid fa-pen">
@@ -46,10 +78,60 @@ const addTask = (event) => {
   `;
 
 
+  const checkbox = li.querySelector('.checkbox');//this ensures proper functionality because it finds the check box and ensures that the checkbox isn't checked before editing. 
+  const editBtn = li.querySelector('.edit-btn');
+
+    if (complete){
+      li.classList.add('completed');
+      editBtn.disabled = true;
+      editBtn.style.opacity = '0.5';
+      editBtn.style.pointerEvents = 'none';
+
+    }//this allows use check if a task has been completed
+
+    checkbox.addEventListener('change', () => {
+       const isChecked = checkbox.checked;
+       li.classList.toggle('completed', isChecked);
+       editBtn.disabled = isChecked;
+       editBtn.style.opacity = isChecked ? '0.5' : '1';
+       editBtn.style.pointerEvents = isChecked ? 'none' : 'auto';
+       updateProgress();
+       saveTaskToLocalStorage();
+
+    });
+
+
+
+  editBtn.addEventListener('click', () => {
+  if(!checkbox.checked){
+    taskInput.value = li.querySelector('span').textContent;
+    li.remove();//This allows for if a task is removed, the existing list item will be removed using li.remove
+    toggleEmptyState();
+    updateProgress(false);
+    saveTaskToLocalStorage();
+
+
+  }
+
+});
+
+li.querySelector('.delete-btn').addEventListener('click', () => {
+  li.remove();
+  toggleEmptyState();
+  updateProgress();
+  saveTaskToLocalStorage();
+
+});
+
+
+
+
   //li.textContent = taskText;
   taskList.appendChild(li);
   taskInput.value = '';
   toggleEmptyState();
+  updateProgress(checkCompletion);
+  saveTaskToLocalStorage();
 
 
 };
@@ -63,11 +145,56 @@ The if statement that is attached to the taskText that will return/stop  if the 
  */
 
 
-addTaskBtn.addEventListener('click', addTask);
+addTaskBtn.addEventListener('click',  () => addTask());
 taskInput.addEventListener('keypress', (e) => {
   if(e.key === 'Enter'){
-    addTask(e);
+  e.preventDefault();
+  addTask();
   }
 });
 
+loadTaskFromLocalStorage();
 });
+
+const Confetti = () => {
+
+  const count = 200,
+  defaults = {
+    origin: { y: 0.7 },
+  };
+
+function fire(particleRatio, opts) {
+  confetti(
+    Object.assign({}, defaults, opts, {
+      particleCount: Math.floor(count * particleRatio),
+    })
+  );
+}
+
+fire(0.25, {
+  spread: 26,
+  startVelocity: 55,
+});
+
+fire(0.2, {
+  spread: 60,
+});
+
+fire(0.35, {
+  spread: 100,
+  decay: 0.91,
+  scalar: 0.8,
+});
+
+fire(0.1, {
+  spread: 120,
+  startVelocity: 25,
+  decay: 0.92,
+  scalar: 1.2,
+});
+
+fire(0.1, {
+  spread: 120,
+  startVelocity: 45,
+});
+}
